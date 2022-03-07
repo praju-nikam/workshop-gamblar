9c49077c1e8970c220020a608f5d70d9190c48f3
praju <sawantprajakta84@gmail.com>
2022-03-07 08:55:18 +0530

    Gambler Program

---
+++ src/com/java/Gambler.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Gambler
+{
+  public static void main(String args[])
+  {
+      int stake = 100 , bet = 1 ;
+      System.out.println("Welcome to Gambler");
+      System.out.println("Stake = " +stake);
+      System.out.println("Bet = " +bet);
+  }
+}
