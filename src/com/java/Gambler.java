ad2caffd030432c56ccfb5b784727fc58d97acd2
praju <sawantprajakta84@gmail.com>
2022-03-07 10:23:45 +0530

    Program is Updated

--- src/com/java/Gambler.java
+++ src/com/java/Gambler.java
@@ -5,10 +5,22 @@ public class Gambler
     public static final int STACK = 100;
     public static final int BET = 1;
 
+    public void WinLose()
+    {
+        boolean check = (Math.random() < 0.5);
+        if(check )
+        {
+            System.out.println("Player Win");
+        }
+        else
+        {
+            System.out.println("Player is Loose");
+        }
+    }
   public static void main(String args[])
   {
       System.out.println("Welcome to Gambler");
-      System.out.println("Stake = " +STACK);
-      System.out.println("Bet = " +BET);
+         Gambler gambler = new Gambler();
+         gambler.WinLose();
   }
 }
