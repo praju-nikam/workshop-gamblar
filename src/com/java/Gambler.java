61dd0f78aa5b213a9dff71026023fb5b3be41eb8
praju <sawantprajakta84@gmail.com>
2022-03-07 10:08:25 +0530

    Program is Updated

--- src/com/java/Gambler.java
+++ src/com/java/Gambler.java
@@ -2,11 +2,13 @@ package com.java;
 
 public class Gambler
 {
+    public static final int STACK = 100;
+    public static final int BET = 1;
+
   public static void main(String args[])
   {
-      int stake = 100 , bet = 1 ;
       System.out.println("Welcome to Gambler");
-      System.out.println("Stake = " +stake);
-      System.out.println("Bet = " +bet);
+      System.out.println("Stake = " +STACK);
+      System.out.println("Bet = " +BET);
   }
 }
